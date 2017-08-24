333e4ab67beb0471c4f30861bba50f6eebbfdd90
akashnits <akash.nits@outlook.com>
2017-08-24 11:48:53 +0530

    Step 1 & 2 completed

--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,11 +1,14 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+import com.example.JokeSupplier;
+import com.example.android.jokedisplaylib.JokeDisplay;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -40,7 +43,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Intent intent= new Intent(this, JokeDisplay.class);
+        intent.putExtra("joke", new JokeSupplier().getJoke());
+        startActivity(intent);
     }
 
 
